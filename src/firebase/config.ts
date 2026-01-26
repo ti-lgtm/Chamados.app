e4a16cb6bd4d86ceb8a81bb682ba76a6f1267d52
Thulio Costa <ti@andrademarinholmf.com.br>
2026-01-26 12:09:17 +0000

    Access to XMLHttpRequest at 'https://firebasestorage.googleapis.com/v0/b

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-6683715047-7b2d5",
   "appId": "1:980840576762:web:f7d84b37d08c125e41fb43",
-  "storageBucket": "studio-6683715047-7b2d5.appspot.com",
+  "storageBucket": "chamadosapp.appspot.com",
   "apiKey": "AIzaSyDwkYQOZ4R4mb5iLJon2-92w5KbO1WNISA",
   "authDomain": "studio-6683715047-7b2d5.firebaseapp.com",
   "measurementId": "",
