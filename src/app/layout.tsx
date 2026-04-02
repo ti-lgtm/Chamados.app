5a55464a338fe9f6b7a017b0fe0afa375fe20798
Thulio Costa <ti@andrademarinholmf.com.br>
2026-04-02 13:55:06 +0000

    Poe essa na logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   description: "Portal de Agendamentos e Chamados de TI",
 };
 
-const favicon = `data:image/svg+xml,%3Csvg viewBox='0 0 52 48' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M26 0L0.908203 22.5V48H51.0918V22.5L26 0ZM13.0918 25.5L26 15L38.9082 25.5V42H13.0918V25.5Z' fill='%23F97316'/%3E%3C/svg%3E`;
+const favicon = `data:image/svg+xml,%3Csvg viewBox='0 0 50 48' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M25%200%20L50%2020%20L45%2048%20L5%2048%20L0%2020%20Z%20M25%208%20L42%2024%20L38%2041%20L12%2041%20L8%2024%20Z' fill='%23F97316'/%3E%3C/svg%3E`;
 
 export default function RootLayout({
   children,
--- src/components/icons/portal-logo.tsx
+++ src/components/icons/portal-logo.tsx
@@ -1,7 +1,7 @@
 export function PortalLogo(props: React.SVGProps<SVGSVGElement>) {
     return (
-        <svg viewBox="0 0 52 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
-             <path fillRule="evenodd" clipRule="evenodd" d="M26 0L0.908203 22.5V48H51.0918V22.5L26 0ZM13.0918 25.5L26 15L38.9082 25.5V42H13.0918V25.5Z" />
+        <svg viewBox="0 0 50 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+             <path fillRule="evenodd" clipRule="evenodd" d="M25 0 L50 20 L45 48 L5 48 L0 20 Z M25 8 L42 24 L38 41 L12 41 L8 24 Z" />
         </svg>
     );
 }
