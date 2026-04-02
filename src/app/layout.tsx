dcb7b399f39e64f57abf38e08ac1efe99b85e4ef
Thulio Costa <ti@andrademarinholmf.com.br>
2026-04-02 14:58:09 +0000

    Corrige a logo não está no centro do circilo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
-        <link rel="icon" href={favicon} type="image/svg+xml" />
+        <link rel="icon" href={favicon} type="/src/app/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
